3846f194e6fd7b22f4546c2194fd53a0971f02e5
Franciszek Gorczyca <franciszekgorczyca@sztarbuks.local>
2023-09-01 14:56:07 +0200

    add more matching instances

--- solution.py
+++ solution.py
@@ -2,6 +2,9 @@ from glob import glob
 from typing import List, Dict, Set
 import re
 
+# https://docs.oracle.com/javase/8/docs/api/java/lang/package-summary.html
+DEFAULT_DECLARATIONS = ["String", "Integer", "Boolean", "Double", "Float", "Number", "Character", "Byte", "Short"]
+
 PACKAGE_NAME = r"(?:package\s*)(.*\.\w+);"
 FILE_NAME = r"(\w+)\.java"
 SINGLE_LINE_COMMENT = r"//.*"
@@ -9,7 +12,10 @@ MULTI_LINE_COMMENT = r"/\*(?:.*\n)*.*\*/"
 FOLDER_IMPORTS = r"(?:import\s+)(.*\.\w+)\.\*;"
 FILE_IMPORTS = r"(?:import\s+)(.*\.\w+);"
 CLASS_OR_INTERFACE_NAME = r"(?:class|interface)\s+(\w+)"
-INSTANCE_CREATION = r"new\s+(\w+)\([\w,]*\);"
+INSTANCE_CREATION = r"\snew\s+(\w+)\([\w,]*\);"
+GENERIC_TYPES = r"(\w+)\s*<\s*(\w*)\s*>(?!\s+\w+\s+\w+)"
+ARRAY_DECLARATIONS = r"(\w+)\s*(?:\[\])+\s*(?:\w+)"
+
 
 def find_files() -> Dict[str, str]:
     file_names = glob("**/*.java", recursive=True)
@@ -77,9 +83,15 @@ def find_file_imports(str: str):
     return set(matches)
 
 
-def find_instance_creations(str: str):
-    matches = re.findall(INSTANCE_CREATION, str)
-    return set(matches)
+def find_none_empty_matches_types(regex: str, content: str):
+    matches = re.findall(regex, content)
+    result = set()
+    for group in matches:
+        for match in [group] if isinstance(group, str) else group:
+            if match == "":
+                continue
+            result.add(match)
+    return set(result)
 
 
 def find_dependency_from_imports(class_names, class_declarations, imported_files):
@@ -96,7 +108,8 @@ def main():
     # When working with a file name we use the fully qualified name
     file_content = find_files() # K: file name, V: contents with package name
     file_depends = {} # K: file name, V: set of dependencies
-    class_declarations = {} # K: file name, V: set declarations
+     # K: file name, V: set declarations
+    class_declarations = { f"java.lang.{declaration}": set([declaration]) for declaration in DEFAULT_DECLARATIONS }
     imported_files = {} # K: file name, V: set of imported files (Including itself)
 
     for k, v in file_content.items():
@@ -109,15 +122,17 @@ def main():
     for k, v in file_content.items():
         folder_imports = find_imports_through_folders(v, file_content.keys())
         package_imports = find_imports_through_package(get_package_name(k), file_content.keys())
-        imported_files[k] = folder_imports.union(package_imports)
+        default_imports = set([f"java.lang.{declaration}" for declaration in DEFAULT_DECLARATIONS])
+        imported_files[k] = folder_imports.union(package_imports).union(default_imports)
 
     for k, v in file_content.items():
         file_depends[k] = find_file_imports(v)
 
     for k, v in file_content.items():
-        classes = find_instance_creations(v)
-        dependencies = find_dependency_from_imports(classes, class_declarations, imported_files)
-        file_depends[k] = file_depends[k].union(dependencies)
+        for regex in [INSTANCE_CREATION, GENERIC_TYPES, ARRAY_DECLARATIONS]:
+            matches = find_none_empty_matches_types(regex, v)
+            dependencies = find_dependency_from_imports(matches, class_declarations, imported_files[k])
+            file_depends[k] = file_depends[k].union(dependencies)
 
     for k, v in file_depends.items():
         file_depends[k].discard(k)
@@ -127,3 +142,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+## like this List <foo> -> is not working
+## same List< foo >
+## if ( a < b && b > c)

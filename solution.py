af22f4183e6846985da05815714de835de9278e8
Noblix <oliver@stenberg-jakobsen.dk>
2023-09-01 09:41:22 +0200

    Find imported files that potentially could be a dependency

--- solution.py
+++ solution.py
@@ -6,6 +6,7 @@ PACKAGE_NAME = r"(?:package\s*)(.*\.\w+);"
 FILE_NAME = r"(\w+)\.java"
 SINGLE_LINE_COMMENT = r"//.*"
 MULTI_LINE_COMMENT = r"/\*(?:.*\n)*.*\*/"
+FOLDER_IMPORTS = r"(?:import\s*)(.*\.\w+)\.\*;"
 FILE_IMPORTS = r"(?:import\s*)(.*\.\w+);"
 CLASS_OR_INTERFACE_NAME = r"(?:class|interface)\s+(\w+)"
 
@@ -25,8 +26,8 @@ def find_files() -> Dict[str, str]:
 
     return files
 
-def fix_file_name():
-    pass
+def get_package_name(file_name: str) -> str:
+    return ".".join(file_name.split(".")[0:-1])
 
 def delete_comments(str: str) -> str:
     str = re.sub(SINGLE_LINE_COMMENT, "", str, count=0)
@@ -35,6 +36,22 @@ def delete_comments(str: str) -> str:
 
 def delete_strings(str: str) -> str:
     pass
+
+def find_imports_through_folders(str: str, file_names: [str]):
+    matches = re.findall(FOLDER_IMPORTS, str)
+    result = set()
+    for match in matches:
+        for file_name in file_names:
+            if file_name.startswith(match):
+                result.add(file_name)
+    return result
+
+def find_imports_through_package(package_name: str, file_names: [str]):
+    result = set()
+    for file_name in file_names:
+        if file_name.startswith(package_name):
+            result.add(file_name)
+    return result
 
 def find_file_imports(str: str):
     matches = re.findall(FILE_IMPORTS, str)
@@ -50,15 +67,21 @@ def main():
     file_content = find_files() # K: file name, V: contents with package name
     file_depends = {} # K: file name, V: list of dependencies
     class_declarations = {} # K: declaration, V: file name
+    imported_files = {} # K: file name, V: set of imported files (Including itself)
 
     for k, v in file_content.items():
         file_content[k] = delete_comments(v)
 
     for k, v in file_content.items():
-        package_name = ".".join(k.split(".")[0:-1])
+        package_name = get_package_name(k)
         declarations = find_declarations(v)
         for declaration in declarations:
             class_declarations[f"{package_name}.{declaration}"] = k # Fully qualified
+
+    for k, v in file_content.items():
+        folder_imports = find_imports_through_folders(v, file_content.keys())
+        package_imports = find_imports_through_package(get_package_name(k), file_content.keys())
+        imported_files[k] = folder_imports.union(package_imports)
 
     for k, v in file_content.items():
         file_depends[k] = find_file_imports(v)

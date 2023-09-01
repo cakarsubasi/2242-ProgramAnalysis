52f6d14c81cc95a10633dc81095eac414377f29f
Noblix <oliver@stenberg-jakobsen.dk>
2023-09-01 10:23:44 +0200

    Reorder methods and add some spacing between

--- solution.py
+++ solution.py
@@ -27,50 +27,10 @@ def find_files() -> Dict[str, str]:
 
     return files
 
+
 def get_package_name(file_name: str) -> str:
     return ".".join(file_name.split(".")[0:-1])
 
-def delete_comments(str: str) -> str:
-    str = re.sub(SINGLE_LINE_COMMENT, "", str, count=0)
-    str = re.sub(MULTI_LINE_COMMENT, "", str, count=0)
-    return str
-
-def find_imports_through_folders(str: str, file_names: List[str]):
-    matches = re.findall(FOLDER_IMPORTS, str)
-    result = set()
-    for match in matches:
-        for file_name in file_names:
-            if file_name.startswith(match):
-                result.add(file_name)
-    return result
-
-def find_imports_through_package(package_name: str, file_names: List[str]):
-    result = set()
-    for file_name in file_names:
-        if file_name.startswith(package_name):
-            result.add(file_name)
-    return result
-
-def find_file_imports(str: str):
-    matches = re.findall(FILE_IMPORTS, str)
-    return set(matches)
-
-def find_instance_creations(str: str):
-    matches = re.findall(INSTANCE_CREATION, str)
-    return set(matches)
-
-def find_declarations(str: str):
-    matches = re.findall(CLASS_OR_INTERFACE_NAME, str)
-    return set(matches)
-
-def find_dependency_from_imports(class_names, class_declarations, imported_files):
-    result = set()
-    for class_name in class_names:
-        for imported_file in imported_files:
-            for declaration in class_declarations[imported_file]:
-                if declaration == class_name:
-                    result.add(imported_file)
-    return result
 
 def create_graphviz_text(file_depends: Dict[str, Set[str]]):
     result = "digraph SourceGraph {"
@@ -81,6 +41,56 @@ def create_graphviz_text(file_depends: Dict[str, Set[str]]):
             result += f"""\n  "{file_name}" -> "{depedency}";"""
     result += "\n}"
     return result
+
+
+def delete_comments(str: str) -> str:
+    str = re.sub(SINGLE_LINE_COMMENT, "", str, count=0)
+    str = re.sub(MULTI_LINE_COMMENT, "", str, count=0)
+    return str
+
+
+def find_declarations(str: str):
+    matches = re.findall(CLASS_OR_INTERFACE_NAME, str)
+    return set(matches)
+
+
+def find_imports_through_folders(str: str, file_names: List[str]):
+    matches = re.findall(FOLDER_IMPORTS, str)
+    result = set()
+    for match in matches:
+        for file_name in file_names:
+            if file_name.startswith(match):
+                result.add(file_name)
+    return result
+
+
+def find_imports_through_package(package_name: str, file_names: List[str]):
+    result = set()
+    for file_name in file_names:
+        if file_name.startswith(package_name):
+            result.add(file_name)
+    return result
+
+
+def find_file_imports(str: str):
+    matches = re.findall(FILE_IMPORTS, str)
+    return set(matches)
+
+
+def find_instance_creations(str: str):
+    matches = re.findall(INSTANCE_CREATION, str)
+    return set(matches)
+
+
+def find_dependency_from_imports(class_names, class_declarations, imported_files):
+    result = set()
+    for class_name in class_names:
+        for imported_file in imported_files:
+            for declaration in class_declarations[imported_file]:
+                if declaration == class_name:
+                    result.add(imported_file)
+    return result
+
 
 def main():
     # When working with a file name we use the fully qualified name
@@ -117,5 +127,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-

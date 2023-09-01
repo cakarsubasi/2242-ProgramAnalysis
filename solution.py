5431c400246292d9942eaaae5b21fec73f7c6fdf
Franciszek Gorczyca <franciszekgorczyca@sztarbuks.local>
2023-09-01 17:29:22 +0200

    handle strings, static methods

--- solution.py
+++ solution.py
@@ -7,9 +7,11 @@ DEFAULT_DECLARATIONS = ["String", "Integer", "Boolean", "Double", "Float", "Numb
 
 PACKAGE_NAME = r"(?:package\s*)(.*\.\w+);"
 FILE_NAME = r"(\w+)\.java"
+STATIC_METHOD_NAME = r"static\s+\w+\s+(\w+)"
 SINGLE_LINE_COMMENT = r"//.*"
 INLINE_COMMENT = r"\/\*(?:.*)\*\/"
 MULTI_LINE_COMMENT = r"/\*(?:.*\n)*.*\*/"
+STRINGS = r'".*"'
 FOLDER_IMPORTS = r"(?:import\s+)(.*\.\w+)\.\*;"
 FILE_IMPORTS = r"(?:import\s+)(.*\.\w+);"
 CLASS_OR_INTERFACE_NAME = r"(?:class|interface)\s+(\w+)"
@@ -43,6 +45,10 @@ def construct_class_name_regex(class_names: Set[str]) -> str:
 def construct_generic_regex_finder(class_names: Set[str]) -> str:
     return fr"<({'|'.join(class_names)}(?:,\s*(?:{'|'.join(class_names)}))*)>\s+\w+\s+\w+\((\w+\s+\w+(?:,\s*\w+\s+\w+)*)\)"
 
+def construct_static_method_regex(function_names: Set[str]) -> str:
+    return r"\b(" + "|".join(function_names) + r")\s*\("
+    
+
 def create_graphviz_text(file_depends: Dict[str, Set[str]]):
     result = "digraph SourceGraph {"
     for file_name in file_depends.keys():
@@ -58,12 +64,18 @@ def delete_comments(str: str) -> str:
     str = re.sub(SINGLE_LINE_COMMENT, "", str, count=0)
     str = re.sub(INLINE_COMMENT, "", str, count=0)
     str = re.sub(MULTI_LINE_COMMENT, "", str, count=0)
+    str = re.sub(STRINGS, '""', str, count=0)
     return str
 
 
 def find_declarations(str: str):
     matches = re.findall(CLASS_OR_INTERFACE_NAME, str)
     return set(matches)
+
+
+def find_static_methods(str: str, file_name: str):
+    matches = re.findall(STATIC_METHOD_NAME, str)
+    return set(f"{file_name}.{match}" for match in matches)
 
 
 def find_imports_through_folders(str: str, file_names: List[str]):
@@ -109,6 +121,21 @@ def find_dependency_from_imports(class_names, class_declarations, imported_files
                     result.add(imported_file)
     return result
 
+def find_generics_that_are_shadowing(generic_regex: str, content: str) -> Set[str]:
+    generic_matches = re.findall(generic_regex, content)
+    shadowing = set()
+    for group in generic_matches:
+        if len(group) < 2:
+            continue
+        
+        generics = list(map(lambda x: x.strip(), group[0].split(",")))
+        parameters = list(map(lambda x: x.strip().split(" ")[0].strip(), group[1].split(",")))
+        for generic in generics:
+            for parameter in parameters:
+                if generic == parameter:
+                    shadowing.add(generic)
+    return shadowing
+
 
 def main():
     # When working with a file name we use the fully qualified name
@@ -118,12 +145,15 @@ def main():
     class_declarations = { f"java.lang.{declaration}": set([declaration]) for declaration in DEFAULT_DECLARATIONS }
     imported_files = {} # K: file name, V: set of imported files (Including itself)
 
+    static_methods = set()
+
     for k, v in file_content.items():
         file_content[k] = delete_comments(v)
 
     for k, v in file_content.items():
         declarations = find_declarations(v)
         class_declarations[k] = declarations
+        static_methods = static_methods.union(find_static_methods(v, k))
 
     for k, v in file_content.items():
         folder_imports = find_imports_through_folders(v, file_content.keys())
@@ -133,6 +163,11 @@ def main():
 
     for k, v in file_content.items():
         file_depends[k] = find_file_imports(v)
+
+    for k, v in file_content.items():
+        matches = re.findall(construct_static_method_regex(static_methods), v)
+        dependencies = set((".").join(match.split(".")[0:-1]) for match in matches)
+        file_depends[k] = file_depends[k].union(dependencies)
 
     for k, v in file_content.items():
         for regex in [INSTANCE_CREATION, GENERIC_TYPES, ARRAY_DECLARATIONS]:
@@ -150,19 +185,8 @@ def main():
         generic_regex = construct_generic_regex_finder(class_names)
 
         dynamic_matches = find_none_empty_matches_types(dynamic_class_regex, v)
-        generic_matches = re.findall(generic_regex, v)
-        shadowing = set()
-        for group in generic_matches:
-            if len(group) < 2:
-                continue
-            
-            generics = list(map(lambda x: x.strip(), group[0].split(",")))
-            parameters = list(map(lambda x: x.strip().split(" ")[0].strip(), group[1].split(",")))
-            for generic in generics:
-                for parameter in parameters:
-                    if generic == parameter:
-                        shadowing.add(generic)
-        
+        shadowing = find_generics_that_are_shadowing(generic_regex, v)
+
         for shadow in shadowing:
             dynamic_matches.discard(shadow)
 

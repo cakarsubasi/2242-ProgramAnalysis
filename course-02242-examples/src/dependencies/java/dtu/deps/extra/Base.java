f700adda9979c1c7d95deeb01cb98164f11353ff
Noblix <oliver@stenberg-jakobsen.dk>
2023-09-15 11:34:24 +0200

    Add more fields to Base.java

--- course-02242-examples/src/dependencies/java/dtu/deps/extra/Base.java
+++ course-02242-examples/src/dependencies/java/dtu/deps/extra/Base.java
@@ -1,5 +1,11 @@
 package dtu.deps.extra;
 
 public class Base {
-    public String name;
+    public String publicName;
+    String packagePrivateName;
+    protected String protectedName;
+    private String privateName;
+    private static String privateStaticName;
+    static String staticName;
+    public Iterable<String> listField;
 }

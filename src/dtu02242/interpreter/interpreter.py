1916a8f80ea4693aa5e31252ecf43b036c35b80b
Noblix <oliver@stenberg-jakobsen.dk>
2023-09-22 12:42:30 +0200

    Add some stronger typing

--- src/dtu02242/interpreter/interpreter.py
+++ src/dtu02242/interpreter/interpreter.py
@@ -70,10 +70,13 @@ class ClassValue:
 # JavaValue can be these things, but feel free to add more
 JavaValue = IntValue | BoolValue | ByteValue | ShortValue | RefValue | None
 
-#class Value:
-#    def __init__(self, type_name: str, value: Any):
-#        self.type_name = type_name
-#        self.value = value
+class Value:
+    def __init__(self, value: Any, type_name: str = None):
+        self.value = value
+        if type_name is None:
+            self.type_name = type(value).__name__ 
+        else:
+            self.type_name = type_name
 
 class JavaError:
     # Need a way of propagating errors
@@ -91,46 +94,59 @@ class Counter:
 
 class StackElement:
 
-    def __init__(self, local_variables, operational_stack, counter: Counter):
-        self.local_variables: List[JavaValue] = local_variables
-        self.operational_stack: List[JavaValue] = operational_stack
+    def __init__(self, local_variables: List[Value], operational_stack, counter: Counter):
+        self.local_variables: List[Value] = local_variables
+        self.operational_stack: List[Value] = operational_stack
         self.counter: Counter = counter
+
+class Operation:
+    def __init__(self, json_doc):
+        self.offset: int = json_doc["offset"]
+        self.opr: str = json_doc["opr"]
+        self.type: str = json_doc["type"] if "type" in json_doc else None
+        self.index: int = json_doc["index"] if "index" in json_doc else None
+        self.operant: str = json_doc["operant"] if "operant" in json_doc else None
+        self.value: Value = Value(json_doc["value"]["value"], json_doc["value"]["type"]) if "value" in json_doc else None
+
+    def get_name(self):
+        if self.operant:
+            return f"{self.opr}-{self.operant}"
+        return self.opr
 
 class Interpreter:
 
-    def __init__(self, java_class, method_name, method_args: List[JavaValue]):
+    def __init__(self, java_class, method_name, method_args: List[Value]):
         self.memory: Dict[str, JavaValue] = {}
         self.stack: List[StackElement] = [StackElement(method_args, [], Counter(method_name, 0))]
         self.java_class = java_class
         #self.method_name = method_name
 
-
     def run(self):
         for _ in range(10):
             element = self.stack[-1]
-            method_bcode = self.java_class.get_method(element.counter.method_name)["code"]["bytecode"][element.counter.counter]
-            if method_bcode["opr"] == "return":
-                return perform_return(self, method_bcode, element)
-            self.run_operation(method_bcode, element)
+            operation = Operation(self.java_class.get_method(element.counter.method_name)["code"]["bytecode"][element.counter.counter])
+            if operation.get_name() == "return":
+                return perform_return(self, operation, element)
+            self.run_operation(operation, element)
 
-    def run_operation(self, operation, element):
-        method = method_mapper[operation["opr"]]
+    def run_operation(self, operation: Operation, element: StackElement):
+        method = method_mapper[operation.get_name()]
         method(self, operation, element)
 
 
-def perform_return(runner: Interpreter, opr: Dict, element: StackElement):
-    type = opr["type"]
+def perform_return(runner: Interpreter, opr: Operation, element: StackElement):
+    type = opr.type
     if type == None:
         return None
-    value = element.operational_stack[-1]
+    value = element.operational_stack[-1].value
     return locate(type)(value)
 
-def perform_push(runner: Interpreter, opr: Dict, element: StackElement):
-    v = opr["value"]
-    runner.stack.append(StackElement(element.local_variables, element.operational_stack + [v["value"]], element.counter.next_counter()))
+def perform_push(runner: Interpreter, opr: Operation, element: StackElement):
+    v = opr.value
+    runner.stack.append(StackElement(element.local_variables, element.operational_stack + [v], element.counter.next_counter()))
 
-def perform_load(runner: Interpreter, opr: Dict, element: StackElement):
-    value = element.local_variables[opr["index"]]
+def perform_load(runner: Interpreter, opr: Operation, element: StackElement):
+    value = element.local_variables[opr.index]
     runner.stack.append(StackElement(element.local_variables, element.operational_stack + [value], element.counter.next_counter()))
     
 method_mapper = {
@@ -138,9 +154,6 @@ method_mapper = {
     "return": perform_return,
     "load": perform_load
 }
-
-#runner = Interpreter(["add", "add"])
-#runner.run()
 
 def run_program(java_program: JavaProgram):
     # Ignore this for now
@@ -154,7 +167,8 @@ def run_method(java_class: JavaClass, method_name: str, method_args: List[JavaVa
 
     # The environment should allow referencing other classes
 
-    interpreter = Interpreter(java_class, method_name, method_args)
+    args = [Value(x) for x in method_args]
+    interpreter = Interpreter(java_class, method_name, args)
     return interpreter.run()
 
 

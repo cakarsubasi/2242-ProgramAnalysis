584b13ac1769b066cc23f8ee3a33806d84032016
RoboAsystent <uselessmail11111@gmail.com>
2023-10-02 10:34:23 +0200

    Add abstract array to state

--- src/dtu02242/prototype-arrays-maks/outOfRange.py
+++ src/dtu02242/prototype-arrays-maks/outOfRange.py
@@ -24,28 +24,6 @@ class JavaClass:
     def __str__(self) -> str:
         return self.json_dict["name"]
 
-# def run_method(java_class: JavaClass, method_name: str, method_args: List[JavaValue], environment: Optional[JavaProgram]=None) -> JavaValue | JavaError:
-#     # This is the entry point, this function should create an
-#     # Interpreter instance, and then run it with the given
-#     # properties. It should raise an error
-#     # if it gets unexpected or inadequate arguments
-
-#     # The environment should allow referencing other classes
-
-#     args = []
-#     memory = {}
-#     for arg in method_args:
-#         type_name = type(arg).__name__
-#         if type_name == "list":
-#             memory_address = uuid.uuid4()
-#             memory[memory_address] = ArrayValue(len(arg), arg)
-#             args.append(Value(memory_address))
-#         else:
-#             args.append(Value(arg, type_name))
-
-#     interpreter = Interpreter(java_class, method_name, args, memory)
-#     return interpreter.run()
-
 class Value:
     def __init__(self, value: Any, type_name: str = None):
         self.value = value
@@ -61,24 +39,27 @@ class AbstractValue:
                 self.max = 2147483647
                 self.min = -2147483646
             if type == "reference":
-                self.min = 0
-                self.max = 4294967295
+                self.null = True
+                self.identifier = None
+
+class AbstractArray:
+    def __init__(self, abstract_len : AbstractValue) :
+        self.length = abstract_len
+        self.values = []
+    
+    length : AbstractValue
+    values : List[AbstractValue]
 
 class AbstractState:
     def __init__ (self, stack : List[AbstractValue], args : List[AbstractValue]) :
         self.stack = stack
         self.args = args
         self.pc = 0
-        self.error = False
-        self.valid_length = False
-        self.err_type = None
+        self.arrays = {}
 
     stack : List[AbstractValue]
     args : List[AbstractValue]
     pc : int
-    error : bool
-    err_type : str
-    valid_length : bool
 
 
 class Abstraction:
@@ -108,21 +89,39 @@ class Abstraction:
         
         if opr["opr"] == "load" :
             new_state.stack.append(state.args.pop(opr["index"]))
+        
         elif opr["opr"] == "push":
             if opr["value"]["type"] == "integer":
                 abstract_value = AbstractValue("integer")
                 abstract_value.min = opr["value"]["value"]
                 abstract_value.max = abstract_value.min
                 new_state.stack.append(abstract_value)
+        
         elif opr["opr"] == "array_load":
             abstract_index = new_state.stack.pop()
             abstract_reference = new_state.stack.pop()
 
-            if (abstract_index.max != abstract_index.min or not(new_state.valid_length)) :
+            if (abstract_index.max != abstract_index.min or len(new_state.arrays) == 0) :
                 print("Here might be out of range exception: ", byte_code[pc], pc, method_name)
-            if (abstract_reference.max != abstract_reference.min):
+            if (abstract_reference.null):
                 print("Here might be null pointer: ", byte_code[pc], pc, method_name)
+
+        elif opr["opr"] == "get":
+            if opr["field"]["name"] == "$assertionsDisabled" :
+                print("Found assertion")
         
+        elif opr["opr"] == "newarray" :
+            abstract_len = new_state.stack.pop()
+            indentifier = "$" + str(len(new_state.arrays))
+            new_state.arrays[indentifier] = AbstractArray(abstract_len)
+            
+            new_reference = AbstractValue("reference")
+            new_reference.identifier = indentifier
+
+            new_state.args.append(new_reference)
+
+            # Add inner interpretation of new array
+
         return (new_state, pc+1)
 
 def bounded_abstract_interpretation( byte_code : JsonDict, method_args: List[Value], max_iter : int, method_name : str) :
@@ -130,6 +129,7 @@ def bounded_abstract_interpretation( byte_code : JsonDict, method_args: List[Val
     abstrac_args = abstract.toAbstractArgs()
     
     initial_state = AbstractState([], abstrac_args)
+
     state_copy = initial_state
 
     for k in range(0, max_iter):
@@ -146,4 +146,5 @@ with open("Array.json", "r") as fp:
     array = [1,2,3]
 
     bounded_abstract_interpretation(java_class.get_method("access")["code"]["bytecode"], [1, array], 10, "access")
-    bounded_abstract_interpretation(java_class.get_method("first")["code"]["bytecode"], array, 10, "first")
+    bounded_abstract_interpretation(java_class.get_method("first")["code"]["bytecode"], [array], 10, "first")
+    bounded_abstract_interpretation(java_class.get_method("newArray")["code"]["bytecode"], [array], 10, "newArray")
